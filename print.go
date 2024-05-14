1f72d9d2d911e3ba2b45c746a643099f5ff9763f
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-05-14 09:46:57 -0600

    commit print functions
    Signed-off-by: Jordan Rash <15827604+jordan-rash@users.noreply.github.com>

---
+++ print.go
@@ -0,0 +1,18 @@
+package handler
+
+import (
+	"fmt"
+	"io"
+)
+
+func printer(src []io.Writer, data ...any) {
+	for _, s := range src {
+		fmt.Fprintln(s, data...)
+	}
+}
+
+func printerf(src []io.Writer, format string, data ...any) {
+	for _, s := range src {
+		fmt.Fprintf(s, format, data...)
+	}
+}
